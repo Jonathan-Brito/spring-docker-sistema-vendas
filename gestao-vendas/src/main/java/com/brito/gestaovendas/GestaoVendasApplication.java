3984820f2bc4622a62f230deaee9cf7113e0f6e3
norberto.enomoto@outlook.com <devjonathanbrito@gmail.com>
2022-01-03 20:30:19 -0300

    Created repository gvendas

---
+++ gestao-vendas/src/main/java/com/brito/gestaovendas/GestaoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.brito.gestaovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVendasApplication.class, args);
+	}
+
+}
---
+++ gestao-vendas/src/test/java/com/brito/gestaovendas/GestaoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brito.gestaovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

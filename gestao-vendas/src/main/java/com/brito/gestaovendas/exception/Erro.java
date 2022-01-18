666f2935dcda98f62091865cdaa7b0a2906642a7
norberto.enomoto@outlook.com <devjonathanbrito@gmail.com>
2022-01-17 23:20:15 -0300

    Configurado classe de erros

---
+++ gestao-vendas/src/main/java/com/brito/gestaovendas/exception/Erro.java
@@ -0,0 +1,21 @@
+package com.brito.gestaovendas.exception;
+
+public class Erro {
+
+	private String msgUsuario;
+	private String msgDesenvolvedor;
+
+	public Erro(String msgUsuario, String msgDesenvolvedor) {
+		this.msgUsuario = msgUsuario;
+		this.msgDesenvolvedor = msgDesenvolvedor;
+	}
+
+	public String getMsgUsuario() {
+		return msgUsuario;
+	}
+
+	public String getMsgDesenvolvedor() {
+		return msgDesenvolvedor;
+	}
+
+}
---
+++ gestao-vendas/src/main/java/com/brito/gestaovendas/exception/GestaoVendasExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.brito.gestaovendas.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler {
+
+	private static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = gerarListaDeErros(ex.getBindingResult());
+		
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> gerarListaDeErros(BindingResult bindingResult){
+		List<Erro> erros = new ArrayList<Erro>();
+		
+		bindingResult.getFieldErrors().forEach(fieldError -> {
+			String msgUsuario = tratarMensagemDeErroParaUsuario(fieldError);
+			String msgDesenvolvedor = fieldError.toString();
+			
+			erros.add(new Erro(msgUsuario, msgDesenvolvedor));
+		});
+		
+		return erros;
+	}
+	
+	private String tratarMensagemDeErroParaUsuario(FieldError fieldError) {
+		if(fieldError.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)) {
+			return fieldError.getDefaultMessage().concat("É obrigatório. ");
+		}
+		
+		return fieldError.toString();
+	}
+}

faa185614ff7cb1337cdca40e067eec9edde210a
norberto.enomoto@outlook.com <devjonathanbrito@gmail.com>
2022-01-18 21:11:55 -0300

    Configurado método handleEmptyResultDataAccessException

--- gestao-vendas/src/main/java/com/brito/gestaovendas/exception/GestaoVendasExceptionHandler.java
+++ gestao-vendas/src/main/java/com/brito/gestaovendas/exception/GestaoVendasExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.brito.gestaovendas.exception;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private static final String CONSTANT_VALIDATION_LENGTH = "length";
 	private static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
 
 	@Override
@@ -25,6 +29,16 @@ public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler
 		List<Erro> erros = gerarListaDeErros(ex.getBindingResult());
 		
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+		String msgUsuario = "Rescurso não encontrado ";
+		String msgDesenvolvedor = ex.toString();
+		
+		List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDesenvolvedor));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	private List<Erro> gerarListaDeErros(BindingResult bindingResult){
@@ -43,6 +57,10 @@ public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler
 	private String tratarMensagemDeErroParaUsuario(FieldError fieldError) {
 		if(fieldError.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)) {
 			return fieldError.getDefaultMessage().concat("É obrigatório. ");
+		}
+		if(fieldError.getCode().equals(CONSTANT_VALIDATION_LENGTH)) {
+			return fieldError.getDefaultMessage().concat(String.format(" deve ter entre %s e %s caracteres. ", 
+					fieldError.getArguments()[2], fieldError.getArguments()[1]));
 		}
 		
 		return fieldError.toString();
